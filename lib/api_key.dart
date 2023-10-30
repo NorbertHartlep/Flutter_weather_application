486658f2fccdbb35f749a2674b79593a4f59643a
Norbert <norbert.hartlep13@gmail.com>
2023-10-30 22:51:00 +0100

    first commit

---
+++ lib/api_key.dart
@@ -0,0 +1,3 @@
+class ApiKey {
+  final String apiKey = '7a9f81154d019aa577baf593b52e475a';
+}
---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloc_weather_app/api_key.dart';
+import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+class WeatherBlocBloc extends Bloc<WeatherBlocEvent,WeatherBlocState>{
+  WeatherBlocBloc() : super(WeatherBlocInitial()){
+    on<FetchWeather>((event,emit) async {
+      emit(WeatherBlocLoading());
+      try{
+        WeatherFactory wf = WeatherFactory(ApiKey().apiKey, language: Language.ENGLISH);
+        Weather weather = await wf.currentWeatherByLocation(event.position.latitude, event.position.longitude);
+        print(weather);
+        emit(WeatherBlocSuccess(weather));
+      }catch (e){
+        emit(WeatherBlocFailure());
+      }
+    }
+    );
+  }
+}
